a1a514cd7ec96f766ce95616bbd8fc4934d637fd
naesheim <hg.nesheim@gmail.com>
2017-04-10 23:48:27 +0200

    newbs

--- scripts/token.sh
+++ scripts/token.sh
@@ -1,1 +1,1 @@
-docker login -u oauth2accesstoken -p $(docker run --rm -ti google/cloud-sdk gcloud auth print-access-token) https://gcr.io
+docker login -u oauth2accesstoken -p $(docker run --rm -i google/cloud-sdk gcloud auth print-access-token) https://gcr.io
